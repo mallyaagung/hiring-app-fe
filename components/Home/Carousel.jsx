300f9c4e164b2bf755051d6908cc1b52e375b9f6
mallyaagung <Mallyaagung@gmail.com>
2022-11-11 03:48:01 +0800

    fix carousel

--- components/Home/Carousel.jsx
+++ components/Home/Carousel.jsx
@@ -50,7 +50,7 @@ function CustomPrevArrow(props) {
 
 export default function Carousel({ worker }) {
   const settings = {
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
